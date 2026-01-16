9b84728a38f18bb437aa36ab7c18f4f6a563f2f7
Charlie <CharlieLSparrow@gmail.com>
2026-01-15 23:11:58 -0700

    Exercise 3 submission

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomMagic = new Random();
+        int magicNumber = randomMagic.Next(1, 101);
+        int guess = 101;
+        Console.WriteLine("Guess that number!");
+
+        while (guess != magicNumber) {
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess) {
+                Console.WriteLine("Too low");
+            } else if (magicNumber < guess) {
+                Console.WriteLine("Too high");
+            } else {
+                Console.WriteLine("That's it!");
+            }
+        }
     }
 }
