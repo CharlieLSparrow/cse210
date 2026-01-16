3470435d475173a1b451c091d334f6d5d98e8da1
Charlie <CharlieLSparrow@gmail.com>
2026-01-16 00:22:12 -0700

    Exercise 4 submission.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,25 @@
 using System;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> entries = new List<int>();
+        int sum = 0;
+        int track = -1;
+        int entriesLength = entries.Count;
+
+        Console.WriteLine("Enter a bunch of numbers one at a time then when you're done enter 0 and I'll give you the average! ");
+        int entry = int.Parse(Console.ReadLine());
+        while (entry != 0) 
+        {
+            entries.Add(entry);
+            sum += entry;
+            entry = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {sum / entries.Count}");
+        Console.WriteLine($"Max: {entries.Max()}");
     }
 }
