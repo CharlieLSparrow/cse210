b18c0514f5e3b19875d84a581c7e67246721496b
Charlie <CharlieLSparrow@gmail.com>
2026-01-16 01:03:21 -0700

    Exercise 4 Added an "Entries so far" feature.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -6,15 +6,15 @@ internal class Program
     {
         List<int> entries = new List<int>();
         int sum = 0;
-        int track = -1;
-        int entriesLength = entries.Count;
 
         Console.WriteLine("Enter a bunch of numbers one at a time then when you're done enter 0 and I'll give you the average! ");
         int entry = int.Parse(Console.ReadLine());
         while (entry != 0) 
         {
-            entries.Add(entry);
             sum += entry;
+            entries.Add(entry);
+            Console.Write("What you've entered so far: ");
+            Console.WriteLine(String.Join(", ", entries));
             entry = int.Parse(Console.ReadLine());
         }
 
