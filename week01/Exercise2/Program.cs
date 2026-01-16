8751927585a02912b87bd8c7d2e79a0457e8a4ef
Charlie <CharlieLSparrow@gmail.com>
2026-01-15 22:42:39 -0700

    Exercise2 I hope the liberties I took are OK.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        string percent = Console.ReadLine();
+        int grade = int.Parse(percent);
+        char letterGrade;
+        bool isPassing;
+        
+        if (grade >= 90) {
+            letterGrade = 'A';
+        } else if (grade >= 80) {
+            letterGrade = 'B';
+        } else if (grade >= 70) {
+            letterGrade = 'C';
+        } else if (grade >= 60) {
+            letterGrade = 'D';
+        } else {
+            letterGrade = 'F';
+        }
+
+        if (grade >= 70) {
+            isPassing = true;
+        } else {
+            isPassing = false;
+        }
+
+        if (isPassing){
+            Console.WriteLine($"Nice work! {letterGrade} means you passed!");
+        } else {
+            Console.WriteLine($"I'm sorry {letterGrade} isn't passing, but you'll get it next time!");
+        }
     }
 }
