b73f3356443895be28fc3e1793fc8849a916b962
Charlie <CharlieLSparrow@gmail.com>
2026-02-07 14:24:24 -0700

    Corrected Program.cs

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,15 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
+        List<Order> orders = new List<Order>();
+
         // Order 1 (USA)
         Address address1 = new Address("123 Canyon Rd", "Provo", "UT", "USA");
         Customer customer1 = new Customer("Charlie Sparrow", address1);
         Order order1 = new Order(customer1);
         order1.AddProduct(new Product("Podcast Mic", "MIC-1001", 129.99, 1));
         order1.AddProduct(new Product("Boom Arm", "ARM-2040", 49.95, 2));
+        orders.Add(order1);
 
         // Order 2 (International)
         Address address2 = new Address("77 Tulipstraat", "Amsterdam", "NH", "Netherlands");
@@ -18,10 +22,13 @@ class Program
         order2.AddProduct(new Product("Guitar Strings", "STR-9000", 9.99, 6));
         order2.AddProduct(new Product("Capo", "CAP-3002", 14.50, 1));
         order2.AddProduct(new Product("Notebook", "NOTE-1100", 6.25, 3));
+        orders.Add(order2);
 
-        DisplayOrder(order1);
-        Console.WriteLine("===================================");
-        DisplayOrder(order2);
+        foreach (Order order in orders)
+        {
+            DisplayOrder(order);
+            Console.WriteLine("===================================");
+        }
     }
 
     static void DisplayOrder(Order order)
